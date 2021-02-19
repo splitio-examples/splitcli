502878b9b2d198d081c907fc61bc3e48fd1f3007
talianassi921 <talianassi@gmail.com>
2021-02-19 14:13:29 -0800

    adding create a split code that is not working yet

--- Create_Split.py
+++ Create_Split.py
@@ -7,24 +7,34 @@ from decouple import config
 def createSplit():
     split_name = input("Enter a name for your Split: ")
     split_description = input("Enter a description for your Split: ")
-    split_traffic_type = "Choose a traffic type"
-    traffic_type_options = ["user", "account", "anon"]
-    traffic_type_option, index = pick(traffic_type_options, split_traffic_type)
-    create_split_response = requests.post(
-        f"https://api.split.io/internal/api/v2/splits/ws/f8aa2660-3f31-11eb-be37-12b057418355/trafficTypes/{traffic_type_option}", headers={
-            'Content-Type': 'application/json',
-            'Authorization': config('ADMIN_API_KEY')
-        }, json={"fields": [
-            {
-                 "name": "name",
-                 "value": split_name
-                 },
-            {
-                "name": "description",
-                "value": split_description
-            }
 
-        ]})
+    workspace_response = requests.get(
+        "https://api.split.io/internal/api/v2/workspaces", headers={
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + config('ADMIN_API_KEY')
+        }
+    )
+
+    workspace_response.status_code
+    workspace_response_data = workspace_response.json()
+
+    if workspace_response != 200:
+        # giving me a keyerror here
+        workspaceid = workspace_response_data['id']
+        print(workspaceid)
+        print("-------")
+    else:
+        print("something")
+        #print(f"obtained workspace id: {workspaceid}")
+
+    create_split_response = requests.post(
+        f"https://api.split.io/internal/api/v2/splits/ws/{workspaceid}/trafficTypes/user", headers={
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer " + config('ADMIN_API_KEY')
+        }, json={
+            "name":  split_name,
+            "description": split_description
+        })
 
     create_split_response.status_code
     create_split_response.json()
