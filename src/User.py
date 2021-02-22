541cc2e21906e743d3feadc4daa00bc38bfde3ba
Micah Silverman <micah@afitnerd.com>
2021-02-22 12:45:54 -0500

    Updated User to not save phone #. renamed json keys for user. renamed some variables.

--- src/User.py
+++ src/User.py
@@ -3,14 +3,13 @@ import os
 
 
 class User(object):
-    def __init__(self, adminapi: str, orgID: str, userID: str, firstname: str, lastname: str, email: str, phone: str):
-        self.adminapi = adminapi
-        self.orgID = orgID
-        self.userID = userID
+    def __init__(self, adminApiKey: str, orgId: str, userId: str, firstname: str, lastname: str, email: str):
+        self.adminApiKey = adminApiKey
+        self.orgId = orgId
+        self.userId = userId
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
-        self.phone = phone
 
     def __str__(self):
         return json.dumps(self.__dict__)
@@ -23,7 +22,7 @@ class User(object):
         try:
             with open(filename, 'r') as f:
                 dictionary = json.load(f)
-                return User(dictionary["adminapi"], dictionary["orgID"], dictionary["userID"], dictionary["firstname"], dictionary["lastname"], dictionary["email"], dictionary["phone"])
+                return User(dictionary["adminApiKey"], dictionary["orgId"], dictionary["userId"], dictionary["firstname"], dictionary["lastname"], dictionary["email"])
         except:
             return None
 
