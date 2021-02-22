11f0beb034719ff1101d5e2ee8b846af8ae2b7af
talianassi921 <talianassi@gmail.com>
2021-02-22 11:55:09 -0800

    create a split working

--- Create_Split.py
+++ Create_Split.py
@@ -4,31 +4,39 @@ from termcolor import colored
 from decouple import config
 
 
+def list_workspaces():
+    workspaces_url = 'https://api.split.io/internal/api/v2/workspaces'
+    response = requests.get(workspaces_url, headers={
+                            "Authorization": "Bearer " + config('ADMIN_API_KEY')})
+    if response.status_code == 200:
+        return response.json()['objects']
+    else:
+        return []
+
+
+def get_workspace(name):
+    workspaces = list_workspaces()
+    workspaces = list(filter(lambda x: x['name'] == name, workspaces))
+    if len(workspaces) == 0:
+        return None
+    else:
+        return workspaces[0]
+
+
 def createSplit():
     split_name = input("Enter a name for your Split: ")
     split_description = input("Enter a description for your Split: ")
 
-    workspace_response = requests.get(
-        "https://api.split.io/internal/api/v2/workspaces", headers={
-            'Content-Type': 'application/json',
-            'Authorization': "Bearer " + config('ADMIN_API_KEY')
-        }
-    )
+    workspace = get_workspace('Default')
+    if workspace == None:
+        print("ERROR")
+        return
+    workspace_id = workspace['id']
 
-    workspace_response.status_code
-    workspace_response_data = workspace_response.json()
-
-    if workspace_response != 200:
-        # giving me a keyerror here
-        workspaceid = workspace_response_data['id']
-        print(workspaceid)
-        print("-------")
-    else:
-        print("something")
-        #print(f"obtained workspace id: {workspaceid}")
+    traffic_type_name = 'user'
 
     create_split_response = requests.post(
-        f"https://api.split.io/internal/api/v2/splits/ws/{workspaceid}/trafficTypes/user", headers={
+        f"https://api.split.io/internal/api/v2/splits/ws/{workspace_id}/trafficTypes/{traffic_type_name}", headers={
             'Content-Type': 'application/json',
             'Authorization': "Bearer " + config('ADMIN_API_KEY')
         }, json={
@@ -42,4 +50,4 @@ def createSplit():
     if create_split_response.status_code != 200:
         print(create_split_response.json())
     else:
-        print(colored("Your Split has been created!"), "green")
+        print(colored("Your Split has been created!", "green"))
