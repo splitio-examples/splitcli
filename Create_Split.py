303787fe3d5fa0f04fb9813a535cc626211f9215
talianassi921 <talianassi@gmail.com>
2021-02-23 14:07:55 -0800

    update create split

--- Create_Split.py
+++ Create_Split.py
@@ -23,26 +23,60 @@ def get_workspace(name):
         return workspaces[0]
 
 
+def toggleSplit(comment):
+    return rampSplit(0, comment)
+
+
+def rampSplit(ramp, comment):
+    return {
+        "treatments": [
+            {
+                "name": "on"
+            },
+            {
+                "name": "off"
+            }
+        ],
+        "defaultTreatment": "off",
+        "baselineTreatment": "off",
+        "rules": [],
+        "defaultRule": [
+            {
+                "treatment": "on",
+                "size": ramp
+            },
+            {
+                "treatment": "off",
+                "size": 100 - ramp
+            }
+        ],
+        "comment": comment
+    }
+
+
 def createSplit():
     split_name = input("Enter a name for your Split: ")
-    split_description = input("Enter a description for your Split: ")
 
-    workspace = get_workspace('Default')
-    if workspace == None:
-        print("ERROR")
-        return
-    workspace_id = workspace['id']
+    print("Select the type of Split")
+    print("1. Toggle Split")
+    selection = input("Selection: ")
+    if selection == "1":
 
-    traffic_type_name = 'user'
+        workspace = get_workspace('Default')
+        if workspace == None:
+            print("ERROR")
+            return
+        workspace_id = workspace['id']
 
-    create_split_response = requests.post(
-        f"https://api.split.io/internal/api/v2/splits/ws/{workspace_id}/trafficTypes/{traffic_type_name}", headers={
-            'Content-Type': 'application/json',
-            'Authorization': "Bearer " + config('ADMIN_API_KEY')
-        }, json={
-            "name":  split_name,
-            "description": split_description
-        })
+        environment_name = "Prod-Default"
+
+        split_data = toggleSplit("Create via Split CLI")
+
+        create_split_response = requests.post(
+            f"https://api.split.io/internal/api/v2/splits/ws/{workspace_id}/{split_name}/environments/{environment_name}", headers={
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + config('ADMIN_API_KEY')
+            }, data=split_data)
 
     create_split_response.status_code
     create_split_response.json()
