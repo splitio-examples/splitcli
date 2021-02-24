602c8ef80def585b7b2e8a97844d7600b2924d18
HJewkes <hjewkes@gmail.com>
2021-02-24 14:10:32 -0800

    Remove movie

--- templates/split_templates.py
+++ templates/split_templates.py
@@ -26,39 +26,3 @@ def rampSplit(ramp, comment):
         ],
         "comment": comment
     }
-
-# def movie():
-#     return {
-#         "name": "movie_filter",
-#         # "environment": {
-#         #     "id": "f8aa2660-3f31-11eb-be37-12b057418355",
-#         #     "name": "Prod-Default"
-#         # },
-#         # "trafficType": {
-#         #     "id": "f8a8ede0-3f31-11eb-be37-12b057418355",
-#         #     "name": "user"
-#         # },
-#         "killed": False,
-#         "treatments": [
-#             {
-#                 "name": "USA",
-#                 "description": "USA Filter"
-#             },
-#             {
-#                 "name": "default",
-#                 "description": ""
-#             }
-#         ],
-#         "defaultTreatment": "USA",
-#         "baselineTreatment": "default",
-#         "trafficAllocation": 100,
-#         "rules": [],
-#         "defaultRule": [
-#             {
-#                 "treatment": "USA",
-#                 "size": 100
-#             }
-#         ],
-#         # "creationTime": 1613767914941,
-#         # "lastUpdateTime": 1613768561090
-#     }
