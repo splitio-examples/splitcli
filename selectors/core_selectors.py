559f0bf7b31b03edf93ffc9e1b5c73964edd5a41
HJewkes <hjewkes@gmail.com>
2021-02-25 00:09:07 -0800

    Split workflow

--- selectors/core_selectors.py
+++ selectors/core_selectors.py
@@ -8,28 +8,25 @@ def selection_environment(workspace_id):
     environments = environments_api.list_environments(workspace_id)
     if len(environments) == 1:
         environment = environments[0]["name"]
-        print(f"The only environment is {environment}")
         return environments[0]
     title = 'Please select your environment'
-    environment, index = pick(environments, title, options_map_func=lambda env: env['name'])
+    environment,_ = pick(environments, title, options_map_func=lambda env: env['name'])
     return environment
 
 def selection_traffic_type(workspace_id):
     traffic_types = traffic_types_api.list_traffic_types(workspace_id)
     if len(traffic_types) == 1:
         traffic_type = traffic_types[0]["name"]
-        print(f"The only traffic type is {traffic_type}")
         return traffic_types[0]
     title = 'Please select your traffic type'
-    traffic_type, index = pick(traffic_types, title, options_map_func=lambda tt: tt['name'])
+    traffic_type,_ = pick(traffic_types, title, options_map_func=lambda tt: tt['name'])
     return traffic_type
 
 def selection_workspace():
     workspaces = workspaces_api.list_workspaces()
     if len(workspaces) == 1:
         workspace = workspaces[0]["name"]
-        print(f"The only workspace is {workspace}")
         return workspaces[0]
     title = 'Please select your workspace'
-    workspace, index = pick(workspaces, title, options_map_func=lambda ws: ws['name'])
+    workspace,_ = pick(workspaces, title, options_map_func=lambda ws: ws['name'])
     return workspace
--- selectors/split_selectors.py
+++ selectors/split_selectors.py
@@ -1,62 +1,158 @@
 from termcolor import colored
 from pick import pick
+import json
 
-from splitio import splits_api
-from splitio import definitions_api
+from splitio import splits_api, definitions_api, environments_api
 from templates import split_templates
 from selectors import core_selectors
 
-def select_split(workspace_id):
-    splits = splits_api.list_splits(workspace_id)
-    splits.append({"name":"Create new Split", "create_new": True})
-    title = "Select Split to Manage"
-    split = pick(splits, title, options_map_func=lambda env: env['name'])
-    return split
+def manage_splits():
+    workspace = core_selectors.selection_workspace()
+    
+    while True:
+        splits = splits_api.list_splits(workspace["id"])
 
-def selection_split_type():
-    print("Select the type of Split")
-    print("1. Toggle Split")
-    selection = input("Selection: ")
-    if selection == "1":
-        return split_templates.toggleSplit("Create via Split CLI")
+        title = "Workspace: " + workspace["name"] + "\n"
+        if len(splits) == 0:
+            title += "No Splits exist yet!"
+        else:
+            title += "Select Split to Manage"
+        
+        options = []
+        for split in splits:
+            option = split
+            option["option_name"] = split["name"]
+            option["operation"] = lambda: manage_split(workspace, split)
+            options.append(option)
+        options.append({ "option_name": "Create a new Split", "operation": lambda: create_split(workspace) })
+        options.append({ "option_name": "Go back", "go_back": True })
+        
+        _, go_back = select(title, options)
+        if go_back:
+            return
 
-def selection_get_split():
-    workspace_id = core_selectors.selection_workspace()['id']
-    environment_name = core_selectors.selection_environment(workspace_id)
-    split_name = input("Enter a name for your Split: ")
-    return definitions_api.get_split_definition(workspace_id, split_name, environment_name)
-
-def selection_create_split():
+def create_split(workspace):
     try:
         split_name = input("Enter a name for your Split: ")
         split_description = input("Enter a description for your Split: ")
-        workspace_id = core_selectors.selection_workspace()['id']
-        traffic_type = core_selectors.selection_traffic_type(workspace_id)
-        environment = core_selectors.selection_environment(workspace_id)
+        traffic_type = core_selectors.selection_traffic_type(workspace["id"])
 
-        split_data = selection_split_type()
-
-        splits_api.create_split(workspace_id, traffic_type["name"], split_name, split_description)
-        definitions_api.create_split_in_environment(workspace_id, environment["name"], split_name, split_data)
-        print(colored("Your Split has been created!", "green"))
+        splits_api.create_split(workspace["id"], traffic_type["name"], split_name, split_description)
+        output_message("Your Split has been created!")
     except Exception as exc:
-        print("Could not create Split")
-        print(exc)
+        output_message("Could not create Split\n" + str(exc))
 
-def selection_kill_split():
+def manage_split(workspace, split):
+    while True:
+        environments = environments_api.list_environments(workspace["id"])
+
+        options = []
+        for environment in environments:
+            definition = get_definition(workspace, split, environment)
+            option = environment
+            if definition == None:
+                option["option_name"] = "Create in " + option["name"]
+            else:
+                option["option_name"] = "Manage in " + option["name"]
+            option["operation"] = lambda: manage_definition(workspace, split, option)
+            option["definition"] = definition
+            options.append(option)
+        options.append({ "option_name": "Delete Split", "operation": lambda: delete_split(workspace, split), "go_back": True })
+        options.append({ "option_name": "Go back", "go_back": True })
+        title = "How can we help you with " + split["name"]
+
+        _, go_back = select(title, options)
+        if go_back:
+            return
+
+def delete_split(workspace, split):
+    title = "Are you sure?"
+    options = [
+        { "option_name": "Yes", "operation": lambda: splits_api.delete_split(workspace["id"], split["name"]) },
+        { "option_name": "No", "go_back": True }
+    ]
+    select(title, options)
+
+def manage_definition(workspace, split, environment):
+    while True:
+        definition = get_definition(workspace, split, environment)
+        title = "Managing " + split["name"] + " in " + environment["name"]
+
+        options = []
+        if definition == None:
+            options.append({ "option_name": "Create", "operation": lambda: create_definition(workspace, split, environment) })
+            options.append({ "option_name": "Go back", "go_back": True })
+        else:
+            title += "\nDefinition: " + json.dumps(definition)
+            options.append({ "option_name": "Update", "operation": option_unavailable })
+            if definition.get("killed", False):
+                options.append({ "option_name": "Restore", "operation": lambda: restore_definition(workspace, split, environment) })
+            else:
+                options.append({ "option_name": "Kill", "operation": lambda: kill_definition(workspace, split, environment) })
+            options.append({ "option_name": "Delete definition", "operation": lambda: delete_definition(workspace, split, environment) })
+            options.append({ "option_name": "Go back", "go_back": True })
+    
+        _, go_back = select(title, options)
+        if go_back:
+            return
+
+def get_definition(workspace, split, environment):
     try:
-        split_name = input("Enter the name of the Split you wish to kill: ")
-        workspace_id = core_selectors.selection_workspace()['id']
-        environment_name = core_selectors.selection_environment(workspace_id)
-
-        definitions_api.kill_split_in_environment(
-            workspace_id, environment_name, split_name)
-        print(colored(f"You killed {split_name}. RIP.", "red"))
+        return definitions_api.get_split_definition(workspace["id"], environment["name"], split["name"])
     except Exception as exc:
-        print("Could not kill Split")
-        print(exc)
+        return None
 
-def selection_promote_split():
+def delete_definition(workspace, split, environment):
+    title = "Are you sure?"
+    options = [
+        { "option_name": "Yes", "operation": lambda: definitions_api.delete_definition(workspace["id"], environment["name"], split["name"]) },
+        { "option_name": "No", "go_back": True }
+    ]
+    select(title, options)
+
+def select_rollout():
+    title = "Select the type of rollout"
+    options = [
+        { "option_name": "Toggled Rollout", "operation": lambda: split_templates.toggleSplit("Create via Split CLI") },
+        { "option_name": "Ramped Rollout", "operation": option_unavailable }
+    ]
+    selection,_ = select(title, options)
+    return selection
+
+def create_definition(workspace, split, environment):
+    try:
+        split_data = select_rollout()
+        definitions_api.create_split_in_environment(workspace["id"], environment["name"], split["name"], split_data)
+        output_message("Your definition has been created!")
+    except Exception as exc:
+        output_message("Could not create Split\n" + str(exc))
+
+def kill_definition(workspace, split, environment):
+    try:
+        definitions_api.kill_split_in_environment(workspace["id"], environment["name"], split["name"])
+        output_message(f"You killed " + split["name"] + " in " + environment["name"] + ". RIP.")
+    except Exception as exc:
+        output_message("Could not kill Split\n" + str(exc))
+
+def restore_definition(workspace, split, environment):
+    try:
+        definitions_api.restore_split_in_environment(workspace["id"], environment["name"], split["name"])
+        output_message(f"You restored " + split["name"] + " in " + environment["name"] + ". It's Alive!!")
+    except Exception as exc:
+        output_message("Could not restore Split\n" + str(exc))
+
+def promote_definition(workspace, split, environment):
     split_name = input("Enter the name of the Split you wish to promote: ")
-    workspace_id = core_selectors.selection_workspace()['id']
-    environment = core_selectors.selection_environment(workspace_id)
+
+def option_unavailable():
+    output_message("Option unavailable", "Back")
+
+# TODO: Replace pick with something that integrates more seamlessly with users entering data
+
+def output_message(message, option="Continue"):
+    pick([option], message)
+
+def select(title, options):
+    selection,_ = pick(options, title, options_map_func=lambda x: x["option_name"])
+    result = selection["operation"]() if "operation" in selection else None
+    return (result, selection.get("go_back", False))
