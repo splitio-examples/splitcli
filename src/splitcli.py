ab7d3c091fc32af9fc619b83e8468aab8fd3ba38
Henry Jewkes <hjewkes@gmail.com>
2021-03-13 20:04:38 -0800

    Update splitcli.py

--- src/splitcli.py
+++ src/splitcli.py
@@ -2,17 +2,8 @@ from art import text2art
 import argparse
 import sys
 
-<<<<<<< HEAD
 from splitio_selectors import split_selectors, segment_selectors, metric_selectors, organization_selectors
 from accounts import signup, signin, user
-=======
-from splitio_selectors.split_selectors import manage_splits
-from splitio_selectors.segment_selectors import manage_segments
-from splitio_selectors.metric_selectors import manage_metrics
-from splitio_selectors.organization_selectors import manage_organization
-from accounts.user import get_user, sign_in
-from accounts import signup
->>>>>>> main
 import config
 from ux import menu, text
 
@@ -20,32 +11,19 @@ from ux import menu, text
 def initial_prompt():
     # Infinite loop is stopped by exit option
     while True:
-<<<<<<< HEAD
         current_user = user.get_user()
         if user != None:
             knownUserPrompt(current_user)
-=======
-        user = get_user()
-        if user != None:
-            knownUserPrompt(user)
->>>>>>> main
         else:
             newUserPrompt()
 
 def knownUserPrompt(user):
     menu.info_message(text2art(f"Hi {user.firstname}!!!"))
     options = [
-<<<<<<< HEAD
         {"option_name": "Manage Splits", "operation": lambda x: split_selectors.manage_splits()},
         {"option_name": "Manage Segments", "operation": lambda x: segment_selectors.manage_segments()},
         {"option_name": "Manage Metrics", "operation": lambda x: metric_selectors.manage_metrics()},
         {"option_name": "Manage Organization", "operation": lambda x: organization_selectors.manage_organization()},
-=======
-        {"option_name": "Manage Splits", "operation": manage_splits},
-        {"option_name": "Manage Segments", "operation": manage_segments},
-        {"option_name": "Manage Metrics", "operation": manage_metrics},
-        {"option_name": "Manage Organization", "operation": manage_organization},
->>>>>>> main
         {"option_name": "Log Out", "operation": lambda: user.delete()},
         {"option_name": "Exit", "operation": exit}
     ]
@@ -57,11 +35,7 @@ def newUserPrompt():
     options = [
         {"option_name": "No, I need to create an account",
             "operation": lambda: signup.create_account()},
-<<<<<<< HEAD
         {"option_name": "Yes, take me to sign in", "operation": lambda: signin.sign_in()},
-=======
-        {"option_name": "Yes, take me to sign in", "operation": lambda: sign_in()},
->>>>>>> main
         {"option_name": "Exit", "operation": exit}
     ]
     title = f"Do you have an existing account?"
